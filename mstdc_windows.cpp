01248022c0686427f0311e3dcef9f05c4b8ca40c
Chris Dragan <chris@chris.dragan.name>
2022-03-01 19:28:08 +0000

    add initializers and extra stubs

--- mstdc_windows.cpp
+++ mstdc_windows.cpp
@@ -13,8 +13,29 @@
 
 int WINAPI WinMain(HINSTANCE hinstance, HINSTANCE hprev_instance, PSTR cmd_line, INT cmd_show);
 
+using PVFV = void (__cdecl*)();
+
+extern "C" {
+    #pragma const_seg(".CRT$XIA")
+    const PVFV __xi_a = nullptr;
+    #pragma const_seg(".CRT$XIZ")
+    const PVFV __xi_z = nullptr;
+    #pragma const_seg(".CRT$XCA")
+    const PVFV __xc_a = nullptr;
+    #pragma const_seg(".CRT$XCZ")
+    const PVFV __xc_z = nullptr;
+}
+
 int __stdcall WinMainCRTStartup()
 {
+    // Call C++ initializers
+    for (const PVFV* init = &__xi_a; init < &__xi_z; ++init)
+        if (*init)
+            (*init)();
+    for (const PVFV* init = &__xc_a; init < &__xc_z; ++init)
+        if (*init)
+            (*init)();
+
     ExitProcess(WinMain(nullptr, nullptr, nullptr, 0));
 }
 
@@ -75,6 +96,7 @@ extern "C" {
 // In 32-bit builds the compiler calls these functions in order to perform 64-bit arithmetic.
 // This code has been copied from GitHub mmozeiko/win32_crt_float.cpp
 #ifdef _M_IX86
+#   include <immintrin.h>
 #   define CRT_LOWORD(x) dword ptr [x+0]
 #   define CRT_HIWORD(x) dword ptr [x+4]
 
@@ -107,6 +129,143 @@ extern "C" {
 
         #undef A
         #undef B
+    }
+
+    __declspec(naked) void _alldiv()
+    {
+        #define DVND    esp + 16      // stack address of dividend (a)
+        #define DVSR    esp + 24      // stack address of divisor (b)
+
+        __asm
+        {
+            push    edi
+            push    esi
+            push    ebx
+
+            ; Determine sign of the result (edi = 0 if result is positive, non-zero
+            ; otherwise) and make operands positive.
+
+            xor     edi, edi        ; result sign assumed positive
+
+            mov     eax, CRT_HIWORD(DVND) ; hi word of a
+            or      eax, eax        ; test to see if signed
+            jge     short L1        ; skip rest if a is already positive
+            inc     edi             ; complement result sign flag
+            mov     edx, CRT_LOWORD(DVND) ; lo word of a
+            neg     eax             ; make a positive
+            neg     edx
+            sbb     eax, 0
+            mov     CRT_HIWORD(DVND), eax ; save positive value
+            mov     CRT_LOWORD(DVND), edx
+L1:
+            mov     eax, CRT_HIWORD(DVSR) ; hi word of b
+            or      eax, eax        ; test to see if signed
+            jge     short L2        ; skip rest if b is already positive
+            inc     edi             ; complement the result sign flag
+            mov     edx, CRT_LOWORD(DVSR) ; lo word of a
+            neg     eax             ; make b positive
+            neg     edx
+            sbb     eax,0
+            mov     CRT_HIWORD(DVSR), eax ; save positive value
+            mov     CRT_LOWORD(DVSR), edx
+L2:
+
+            ;
+            ; Now do the divide.  First look to see if the divisor is less than 4194304K.
+            ; If so, then we can use a simple algorithm with word divides, otherwise
+            ; things get a little more complex.
+            ;
+            ; NOTE - eax currently contains the high order word of DVSR
+            ;
+
+            or      eax, eax        ; check to see if divisor < 4194304K
+            jnz     short L3        ; nope, gotta do this the hard way
+            mov     ecx, CRT_LOWORD(DVSR) ; load divisor
+            mov     eax, CRT_HIWORD(DVND) ; load high word of dividend
+            xor     edx, edx
+            div     ecx             ; eax <- high order bits of quotient
+            mov     ebx, eax        ; save high bits of quotient
+            mov     eax, CRT_LOWORD(DVND) ; edx:eax <- remainder:lo word of dividend
+            div     ecx             ; eax <- low order bits of quotient
+            mov     edx, ebx        ; edx:eax <- quotient
+            jmp     short L4        ; set sign, restore stack and return
+
+            ;
+            ; Here we do it the hard way.  Remember, eax contains the high word of DVSR
+            ;
+
+L3:
+            mov     ebx, eax        ; ebx:ecx <- divisor
+            mov     ecx, CRT_LOWORD(DVSR)
+            mov     edx, CRT_HIWORD(DVND) ; edx:eax <- dividend
+            mov     eax, CRT_LOWORD(DVND)
+L5:
+            shr     ebx, 1          ; shift divisor right one bit
+            rcr     ecx, 1
+            shr     edx, 1          ; shift dividend right one bit
+            rcr     eax, 1
+            or      ebx, ebx
+            jnz     short L5        ; loop until divisor < 4194304K
+            div     ecx             ; now divide, ignore remainder
+            mov     esi, eax        ; save quotient
+
+            ;
+            ; We may be off by one, so to check, we will multiply the quotient
+            ; by the divisor and check the result against the orignal dividend
+            ; Note that we must also check for overflow, which can occur if the
+            ; dividend is close to 2**64 and the quotient is off by 1.
+            ;
+
+            mul     CRT_HIWORD(DVSR) ; QUOT * CRT_HIWORD(DVSR)
+            mov     ecx, eax
+            mov     eax, CRT_LOWORD(DVSR)
+            mul     esi             ; QUOT * CRT_LOWORD(DVSR)
+            add     edx, ecx        ; EDX:EAX = QUOT * DVSR
+            jc      short L6        ; carry means Quotient is off by 1
+
+            ;
+            ; do long compare here between original dividend and the result of the
+            ; multiply in edx:eax.  If original is larger or equal, we are ok, otherwise
+            ; subtract one (1) from the quotient.
+            ;
+
+            cmp     edx, CRT_HIWORD(DVND) ; compare hi words of result and original
+            ja      short L6        ; if result > original, do subtract
+            jb      short L7        ; if result < original, we are ok
+            cmp     eax, CRT_LOWORD(DVND) ; hi words are equal, compare lo words
+            jbe     short L7        ; if less or equal we are ok, else subtract
+L6:
+            dec     esi             ; subtract 1 from quotient
+L7:
+            xor     edx, edx        ; edx:eax <- quotient
+            mov     eax, esi
+
+            ;
+            ; Just the cleanup left to do.  edx:eax contains the quotient.  Set the sign
+            ; according to the save value, cleanup the stack, and return.
+            ;
+
+L4:
+            dec     edi             ; check to see if result is negative
+            jnz     short L8        ; if EDI == 0, result should be negative
+            neg     edx             ; otherwise, negate the result
+            neg     eax
+            sbb     edx, 0
+
+            ;
+            ; Restore the saved registers and return.
+            ;
+
+L8:
+            pop     ebx
+            pop     esi
+            pop     edi
+
+            ret     16
+        }
+
+        #undef DVND
+        #undef DVSR
     }
 
    __declspec(naked) void _aulldiv()
@@ -338,11 +497,9 @@ RETZERO:
         }
     }
 
-    __declspec(naked) void _ultod3()
+    uint32_t _ultod3(double value)
     {
-        __asm
-        {
-        }
+        return static_cast<uint32_t>(_mm_cvttsd_si32(_mm_set_sd(value)));
     }
 
 #endif
