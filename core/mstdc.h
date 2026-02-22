d23084e326bbe57c11366881874e3a64f103e75d
Chris Dragan <chris@dragan.dev>
2026-02-22 11:29:25 +0000

    core: add DEFER macro

--- core/mstdc.h
+++ core/mstdc.h
@@ -2,6 +2,7 @@
 // SPDX-FileCopyrightText: Copyright (c) 2021-2026 Chris Dragan
 
 #include <stdint.h>
+#include <utility>
 
 // Workaround windows headers
 #ifdef min
@@ -36,4 +37,29 @@ constexpr T align_up(T value, T alignment)
 
 float exp2(float x);
 
+template<typename F>
+class Deferred {
+    public:
+        explicit Deferred(F&& func) : func_(std::move(func)) { }
+        ~Deferred() { func_(); }
+
+        Deferred(const Deferred&)            = delete;
+        Deferred& operator=(const Deferred&) = delete;
+
+    private:
+        F func_;
+};
+
+struct Deferrer {
+    template<typename F>
+    Deferred<F> operator=(F&& func) const
+    {
+        return Deferred<F>(std::forward<F>(func));
+    }
+};
+
 }
+
+#define DEFER_CONCAT_IMPL(a, b) a##b
+#define DEFER_CONCAT(a, b) DEFER_CONCAT_IMPL(a, b)
+#define DEFER auto DEFER_CONCAT(defer_, __LINE__) = mstd::Deferrer{} = [&]()
